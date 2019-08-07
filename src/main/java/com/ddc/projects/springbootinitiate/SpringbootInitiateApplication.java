a38d30fb5737abe790681575000351a8f2ff2da0
丁德超 <dingdechao@dingdechaodeMacBook-Pro.local>
2019-08-08 00:26:37 +0800

    init springboot project

---
+++ src/main/java/com/ddc/projects/springbootinitiate/SpringbootInitiateApplication.java
@@ -0,0 +1,13 @@
+package com.ddc.projects.springbootinitiate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootInitiateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootInitiateApplication.class, args);
+	}
+
+}
